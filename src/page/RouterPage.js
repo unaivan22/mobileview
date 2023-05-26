925740787f00665f75b615a242c36b9a86cb73a2
unaivan <unaivan@gmail.com>
2023-05-26 15:18:45 +0700

    update detail

--- src/page/RouterPage.js
+++ src/page/RouterPage.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Mobileviewone from './v1/Mobileviewone'
 import V1Detail from './v1/utils/V1Detail'
+import Channel from './v1/goal/Channel'
 
 export default function RouterPage() {
   return (
@@ -11,6 +12,8 @@ export default function RouterPage() {
             <Route path='/' element={<Home/>} />
             <Route path='/v1' element={<Mobileviewone/>} />
             <Route path='/v1/detail' element={<V1Detail/>} />
+
+            <Route path='/v1/goal' element={<Channel/>} />
 
         </Routes>
      </Router>
