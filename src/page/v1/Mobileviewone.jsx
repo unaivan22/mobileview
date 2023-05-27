26d10ecde89392fadbfd103c163dee04afc72113
unaivan22 <unaivan22@gmail.com>
2023-05-27 10:24:44 +0700

    local

--- src/page/v1/Mobileviewone.jsx
+++ src/page/v1/Mobileviewone.jsx
@@ -10,18 +10,20 @@ export default function Mobileviewone() {
     <div>
       <Phone>
         <AnimatedPage>
-          <div className='baseHome relative bg-white h-[712px] overflow-y-scroll overflow-x-hidden w-safearea'>
+          <div className='baseHome relative bg-white h-[712px] overflow-y-scroll overflow-x-hidden w-safearea mb-80'>
             <div className='xs:container'>
-              <div className="relative mt-2 rounded-md shadow-sm mx-4">
-                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 text-gray-500">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                </svg>
+              <section className='bg-white sticky top-0 w-full pt-4 pb-2'>
+                <div className="relative rounded-md shadow-sm mx-4">
+                  <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 text-gray-500">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                  </svg>
+                  </div>
+                  <input type="text" name="price" id="price" className="block w-full rounded-lg border-0 py-1.5 pl-8 pr-20 text-gray-900  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-50 text-xs" placeholder="Search" />
                 </div>
-                <input type="text" name="price" id="price" className="block w-full rounded-lg border-0 py-1.5 pl-8 pr-20 text-gray-900  placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 bg-gray-50 text-xs" placeholder="Search" />
-              </div>
+              </section>
 
-              <section className='mt-6'>
+              <section className='mt-4'>
                 <h1 className='font-bold text-lg mx-4 text-zinc-800'>Channel</h1>
                 <div className='w-safearea overflow-x-scroll mobileXscroll scrollerX'>
                   <V1HotNews />
@@ -29,7 +31,10 @@ export default function Mobileviewone() {
               </section>
 
               <section className='mt-6'>
-                <h1 className='font-bold text-lg mx-4 text-zinc-800'>Popular</h1>
+                <div className='flex justify-between items-center'>
+                  <h1 className='font-bold text-lg mx-4 text-zinc-800'>Popular</h1>
+                  <h1 className='font-bold text-xs mx-4 text-rose-500'>See all</h1>
+                </div>
                 <div className='w-safearea overflow-x-scroll mobileXscroll scrollerX'>
                   <V1PopularNews />
                 </div>
@@ -41,6 +46,16 @@ export default function Mobileviewone() {
                   <V1RecentNews />
                 </div>
               </section>
+
+              <div className="relative rounded-xl overflow-auto p-8"><div className="flex items-center justify-center">
+                <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed" disabled="">
+                  <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                  </svg>
+                  Processing...
+                </button>
+              </div></div>
               
             </div>
           </div>
--- src/page/v1/utils/V1Detail.js
+++ src/page/v1/utils/V1Detail.js
@@ -13,14 +13,14 @@ export default function V1Detail() {
                 <h1 className='text-zinc-900 font-bold text-xl leading-snug mb-8'>Richarlison, Antony, Brenden Aaronson & the 21 most disappointing Premier League signings of the 2022-23 season - ranked</h1>
                 <img className='rounded-xl mb-8' src='https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltc535ac4b92498d18/646ca306291f1fe7a0dcf18f/EPL_Most_Disappointing_Signing.jpg?auto=webp&format=pjpg&width=640&quality=60'/>
 
-                <section className='space-y-2'>
-                    <p className='text-sm text-zinc-500 font-light'>GOAL ranks the most underwhelming Premier League transfers of the campaign after record-breaking spending by English clubs</p>
-                    <p className='text-sm text-zinc-500 font-light'>When it comes to transfers, it was a truly remarkable year in the Premier League, as spending across the division reached never-before-seen levels. It wasn't just the usual suspects who splashed the cash, either, with the likes of Newcastle, West Ham and Wolves going big, too.</p>
-                    <p className='text-sm text-zinc-500 font-light'>Indeed, Premier League clubs obliterated the previous record for expenditure across two outrageous transfer windows; £2.1 billion ($2.6bn) was splurged in the summer alone, with that figure rising to £2.8bn ($3.5bn) in January as Chelsea in particular went on a shopping spree.</p>
-                    <p className='text-sm text-zinc-500 font-light'>But with such a phenomenal amount of money spent, there has inevitably been some big misses among the hoard of incoming transfers. Below, GOAL ranks the 21 most disappointing Premier League transfers of 2022-23...</p>
-                    <p className='text-sm text-zinc-500 font-light'>aaa</p>
-                    <p className='text-sm text-zinc-500 font-light'>aaa</p>
-                    <p className='text-sm text-zinc-500 font-light'>aaa</p>
+                <section className='space-y-4'>
+                    <p className='text-xs text-zinc-500 font-light'>GOAL ranks the most underwhelming Premier League transfers of the campaign after record-breaking spending by English clubs</p>
+                    <p className='text-xs text-zinc-500 font-light'>When it comes to transfers, it was a truly remarkable year in the Premier League, as spending across the division reached never-before-seen levels. It wasn't just the usual suspects who splashed the cash, either, with the likes of Newcastle, West Ham and Wolves going big, too.</p>
+                    <p className='text-xs text-zinc-500 font-light'>Indeed, Premier League clubs obliterated the previous record for expenditure across two outrageous transfer windows; £2.1 billion ($2.6bn) was splurged in the summer alone, with that figure rising to £2.8bn ($3.5bn) in January as Chelsea in particular went on a shopping spree.</p>
+                    <p className='text-xs text-zinc-500 font-light'>But with such a phenomenal amount of money spent, there has inevitably been some big misses among the hoard of incoming transfers. Below, GOAL ranks the 21 most disappointing Premier League transfers of 2022-23...</p>
+                    <p className='text-xs text-zinc-500 font-light'>aaa</p>
+                    <p className='text-xs text-zinc-500 font-light'>aaa</p>
+                    <p className='text-xs text-zinc-500 font-light'>aaa</p>
                 </section>
             </div>
         </AnimatedPage>
