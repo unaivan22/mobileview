32649d745b567bbf5efa8ebb1b0ec2bc1747e1f9
unaivan <unaivan@gmail.com>
2023-06-06 09:33:23 +0700

    fixed font size

--- src/page/v2/utils/V2BottomNav.js
+++ src/page/v2/utils/V2BottomNav.js
@@ -40,7 +40,7 @@ export default function V2BottomNav() {
               {({ selected }) => (
                 <button
                   className={
-                    selected ? 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-rose-500 rounded-xl bg-white' : 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-zinc-400 rounded-xl bg-white'
+                    selected ? 'text-center flex flex-col items-center text-xs font-semibold px-4 py-2 text-rose-500 rounded-xl bg-white' : 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-zinc-400 rounded-xl bg-white'
                   }
                 >
                     <BiHomeAlt fontSize="1.5em" />
@@ -53,7 +53,7 @@ export default function V2BottomNav() {
               {({ selected }) => (
                 <button
                   className={
-                    selected ? 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-rose-500 rounded-xl bg-white' : 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-zinc-400 rounded-xl bg-white'
+                    selected ? 'text-center flex flex-col items-center text-xs font-semibold px-4 py-2 text-rose-500 rounded-xl bg-white' : 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-zinc-400 rounded-xl bg-white'
                   }
                 >
                     <AiOutlineSearch fontSize="1.5em" />
@@ -66,7 +66,7 @@ export default function V2BottomNav() {
               {({ selected }) => (
                 <button
                   className={
-                    selected ? 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-rose-500 rounded-xl bg-white' : 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-zinc-400 rounded-xl bg-white'
+                    selected ? 'text-center flex flex-col items-center text-xs font-semibold px-4 py-2 text-rose-500 rounded-xl bg-white' : 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-zinc-400 rounded-xl bg-white'
                   }
                 >
                     <BiBookmark fontSize="1.5em" />
@@ -79,7 +79,7 @@ export default function V2BottomNav() {
               {({ selected }) => (
                 <button
                   className={
-                    selected ? 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-rose-500 rounded-xl bg-white' : 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-zinc-400 rounded-xl bg-white'
+                    selected ? 'text-center flex flex-col items-center text-xs font-semibold px-4 py-2 text-rose-500 rounded-xl bg-white' : 'text-center flex flex-col items-center text-xs font-normal px-4 py-2 text-zinc-400 rounded-xl bg-white'
                   }
                 >
                     <BiMessageSquareDots fontSize="1.5em" />
